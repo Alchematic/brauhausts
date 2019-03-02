f88d2c22c1db740a5b7f4811be6d6e99e387feae
Luke Brown <worbunkel@gmail.com>
2019-03-02 13:36:18 -0600

    Add duration and change to snapshot tests

--- src/lib/recipe-timeline.spec.ts
+++ src/lib/recipe-timeline.spec.ts
@@ -4,21 +4,21 @@ import { calculateRecipe, createRecipe, Recipe } from './recipe';
 import { computeRecipeTimeline } from './recipe-timeline';
 
 describe('computeRecipeTimeline', () => {
-  it('should return the same timeline that brauhaus returns for realRecipe', () => {
-    const recipe = createRecipe(testRecipe2.original as Recipe);
+  it('should return the same timeline that brauhaus returns for realRecipe2', () => {
+    const recipe = createRecipe(testRecipe2 as Recipe);
     const calculatedRecipe = calculateRecipe(recipe);
 
     const result = computeRecipeTimeline(calculatedRecipe);
 
-    expect(result).toEqual(testRecipe2.timeline);
+    expect(result).toMatchSnapshot();
   });
 
   it('should return the same timeline that brauhaus returns for realRecipe3', () => {
-    const recipe = createRecipe(testRecipe3.original as Recipe);
+    const recipe = createRecipe(testRecipe3 as Recipe);
     const calculatedRecipe = calculateRecipe(recipe);
 
     const result = computeRecipeTimeline(calculatedRecipe);
 
-    expect(result).toEqual(testRecipe3.timeline);
+    expect(result).toMatchSnapshot();
   });
 });
